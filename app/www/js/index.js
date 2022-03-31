34ca27a3f98a6af257ced5fa550c04b9dcb69b4c
Dario Salvi <dariosalvi78@gmail.com>
2022-03-31 16:47:59 +0200

    working data capture

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -8,14 +8,16 @@ document.addEventListener('deviceready', onDeviceReady, false)
 document.getElementById('buttonStart').onclick = startCapture
 document.getElementById('buttonStop').onclick = stopCapture
 let globalFileEntry
-let startTimeStamp = 0
+let startTimeStamp, frameTimeStamp
 let accx, accy, accz
 let accGx, accGy, accGz
 
-/*  Variable 'options' sets the options for the CanvasCameraPlugin.
-* Creates timestamp before image is drawn in "onBeforeDraw".
-* Calculates the RGB values after image is drawn in "onAfterDraw".
- */
+let USE_FLASH = true
+let started = false
+
+/* 
+* Variable 'options' sets the options for the CanvasCameraPlugin.
+*/
 var options = {
     CameraFacing: "back",
     canvas: {
@@ -25,15 +27,16 @@ var options = {
         width: 350, height: 350
     },
     fps: 30,
-    flashMode: true,
+    flashMode: USE_FLASH,
     use: 'data',     //Needs to be 'data', 'file' didn't work for IOS devices.
-    onBeforeDraw: function (frame) {
-        startTimeStamp = Date.now()
-    },
-    onAfterDraw: function (frame) {
+    onBeforeDraw: async function (frame) {
         let lum = getLuminosity(frame.element.getContext('2d'))
-        let line = lum + " " + Date.now() + "\n"
-        appendToFile(line)
+        let ts = frameTimeStamp - startTimeStamp
+        let line = ts + ', ' + lum + ', ' + accx + ', ' + accy + ', ' + accz + ', ' + accGx + ', ' + accGy + ', ' + accGz + '\n'
+        await appendToFile(line)
+    },
+    onAfterDraw: async function (frame) {
+
     }
 };
 
@@ -68,13 +71,12 @@ async function createFile () {
 
     return new Promise((resolve, reject) => {
         window.resolveLocalFileSystemURL(folderPath, function (dirEntry) {
-            console.log('file system open: ' + dirEntry.name)
             var tempName = Date.now() + '.csv' //Name of the file
             dirEntry.getFile(tempName, { create: true, exclusive: false }, function (fileEntry) {
                 console.log("file created", tempName)
                 globalFileEntry = fileEntry
+                resolve()
             }, reject)
-            resolve()
         }, reject)
     })
 }
@@ -84,19 +86,31 @@ async function createFile () {
 * Start capturing data.
 */
 async function startCapture () {
+    if (started) return
+
     console.log("Starting capture")
 
     await createFile()
+    let line = 'timestamp , luminance, accX, accY , accZ, accXwithG, accYwithG, accZwithG\n'
+    await appendToFile(line)
 
-    return new Promise((resolve, reject) => {
+    startTimeStamp = Date.now()
+
+    return new Promise(async (resolve, reject) => {
         if (typeof (DeviceMotionEvent.requestPermission) === 'function') {
             await DeviceMotionEvent.requestPermission()
         }
         window.addEventListener('devicemotion', motionHandler, false)
 
         CanvasCamera.start(options, reject, function (data) {
-            window.plugin.CanvasCamera.flashMode(true) //Flashlight would not stay on when IOS device was used. Quickfix.
-            resolve()
+            if (data && data.output && data.output.images && data.output.images.fullsize) {
+                frameTimeStamp = data.output.images.fullsize.timestamp
+            }
+            // if (USE_FLASH) window.plugin.CanvasCamera.flashMode(true) //Flashlight would not stay on when IOS device was used. Quickfix.
+            if (!started) {
+                started = true
+                resolve()
+            }
         })
     })
 }
@@ -105,19 +119,25 @@ async function startCapture () {
 * Stops the capturing.
 */
 async function stopCapture () {
+    if (!started) return
+
     return new Promise((resolve, reject) => {
         window.removeEventListener('devicemotion', motionHandler)
-        window.plugin.CanvasCamera.stop(reject, resolve)
+        window.plugin.CanvasCamera.stop(reject, () => {
+            console.log('Capture stopped')
+            started = false
+            resolve()
+        })
     })
 }
 
 function motionHandler (event) {
-    accx = event.acceleration.x
-    accy = event.acceleration.y
-    accz = event.acceleration.z
-    accGx = event.accelerationIncludingGravity.x
-    accGy = event.accelerationIncludingGravity.y
-    accGz = event.accelerationIncludingGravity.z
+    accx = event.acceleration.x.toFixed(4)
+    accy = event.acceleration.y.toFixed(4)
+    accz = event.acceleration.z.toFixed(4)
+    accGx = event.accelerationIncludingGravity.x.toFixed(4)
+    accGy = event.accelerationIncludingGravity.y.toFixed(4)
+    accGz = event.accelerationIncludingGravity.z.toFixed(4)
 }
 
 
